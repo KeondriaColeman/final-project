d2118b780bf2a02b91c0deb2d28fd3eaeca466ca
Keondria Coleman <Keondria.d.coleman@gmail.com>
2026-08-08 20:38:37 -0500

    fix design

---
+++ script.js
@@ -0,0 +1,7 @@
+function openMenu() {
+    document.getElementById("menu").classList.add("menu--open");
+}
+
+function closeMenu() {
+    document.getElementById("menu").classList.remove("menu--open");
+}
